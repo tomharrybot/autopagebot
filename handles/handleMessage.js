86d0256a2b0c11ed423d5a6b2a0bb2f89a7cc02e
tomharrybot <tomharrybott@gmail.com>
2025-09-08 16:46:45 +0500

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -23,7 +23,7 @@ fs.readdirSync(path.join(__dirname, '../commands'))
   });
 
 // Internal API endpoints
-const INTERNAL_API_BASE = 'https://github.com/wedfhujkkmhhgg233/databasefbpage'; //put your own database url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
+const INTERNAL_API_BASE = 'https://autopagebot-p7gv.onrender.com'; //put your own database url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
 const ENDPOINT_FIND = `${INTERNAL_API_BASE}/find?json=`;
 
 /**
